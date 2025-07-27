93e790fd3b7306c0783365ab2c92131893f17a43
MrStepanAlexandrovich <stepangolenduhin@yandex.ru>
2025-07-27 19:46:33 +0500

    Id задач теперь счётчик, добавлена загрузка/сохранение задач

--- src/Main.java
+++ src/Main.java
@@ -1,47 +1,6 @@
-import manager.FileBackedTaskManager;
-import manager.Managers;
-import manager.TaskManager;
-import task.Epic;
-import task.Status;
-import task.Subtask;
-import task.Task;
-
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-
 public class Main {
 
-    public static void main(String[] args) {/*
-        TaskManager taskManager = Managers.getDefault();
-        int idTask1 = taskManager.addTask(new Task("Zadanie", "descrip", Status.NEW));
-        int idEpic1 = taskManager.addEpic(new Epic("Epichnoe zadanie", "desc", new ArrayList<>()));
-        int idEpic2 = taskManager.addEpic(new Epic("Epichnoe zad2", "desc", new ArrayList<>()));
-        int idEpic = taskManager.addEpic(new Epic("Epichnoe zada1", "desc", new ArrayList<>()));
-        int idSubtask = taskManager.addSubtask(new Subtask("nameSUb", "desc", Status.NEW, taskManager.getEpic(idEpic)));
+    public static void main(String[] args) {
 
-        taskManager.getEpic(idEpic1);
-        taskManager.getEpic(idEpic2);
-        taskManager.getTask(idTask1);
-
-        for (Task task : taskManager.getHistory()) {
-            System.out.println(task);
-        }
-
-        taskManager.getSubtask(idSubtask);
-        System.out.println();
-
-        for (Task task : taskManager.getHistory()) {
-            System.out.println(task);
-        }
-
-*/
-        File file = Paths.get("backup.txt").toFile();
-        TaskManager taskManager = Managers.loadFromFile(file);
     }
 }
