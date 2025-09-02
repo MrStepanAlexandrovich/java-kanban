d369e070aceca7825f63dffc8fad792417036ff1
mr.Stepan <golenduhinsa@gmail.com>
2025-09-02 20:13:48 +0500

    -добавил модификаторы доступа

--- src/task/Task.java
+++ src/task/Task.java
@@ -13,9 +13,6 @@ public class Task {
     private LocalDateTime startTime;
     private Duration duration;
 
-    public Task() {
-    }
-
     public Task(String name, String description, Status status, LocalDateTime startTime, Duration duration) {
         if (name != null && status != null) {
             this.name = name;
